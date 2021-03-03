ed2146a283faa317d1d6f2d07e68c03a1f96bac9
Casey Warren <caseywar1017@gmail.com>
2021-03-02 20:43:00 -0500

    sign up and sign in working. list page rendering

--- src/App.js
+++ src/App.js
@@ -1,12 +1,80 @@
 import React, { Component } from 'react'
 import './App.css';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+} from 'react-router-dom';
+import Home from './Home/Home.js';
+import Header from './components/Header.js';
 
+import TodosListPage from './TodosListPage/TodosListPage.js';
+import SignUpPage from './AuthPages/SignUpPage.js';
+import LoginPage from './AuthPages/LoginPage.js';
+
+
+
+import { getUserFromLocalStorage, putUserInLocalStorage } from './local-storage-utils';
 
 export default class App extends Component {
+  state = {
+    user: getUserFromLocalStorage()
+  }
+
+  handleUserChange = (user) => {
+    this.setState({ user })
+
+    putUserInLocalStorage(user);
+  }
+
+  handleLogout = () => {
+    this.handleUserChange();
+  }
+
   render() {
+    const { user } = this.state;
     return (
       <div>
-
+        <Router>
+          <Header
+            user={this.state.user}
+            handleLogout={this.handleLogout} />
+          <Switch>
+            <Route
+              path="/"
+              exact
+              render={(routerProps) => <Home {...routerProps} />}
+            />
+            <Route
+              path="/todos"
+              exact
+              token={user && user.token}
+              render={(routerProps) =>
+                <TodosListPage
+                  user={this.state.user}
+                  {...routerProps}
+                />}
+            />
+            <Route
+              path="/login"
+              exact
+              render={(routerProps) =>
+                <LoginPage
+                  handleUserChange={this.handleUserChange}
+                  {...routerProps}
+                />}
+            />
+            <Route
+              path="/signup"
+              exact
+              render={(routerProps) =>
+                <SignUpPage
+                  handleUserChange={this.handleUserChange}
+                  {...routerProps}
+                />}
+            />
+          </Switch>
+        </Router>
       </div>
     )
   }
